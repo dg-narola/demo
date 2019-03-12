936904700722827e9083cf21b8d50c8dd3976a77
dg-narola <dg@narola.email>
2019-03-12 17:10:16 +0530

    new

--- app/controllers/sharenotes_controller.rb
+++ app/controllers/sharenotes_controller.rb
@@ -27,7 +27,7 @@ class SharenotesController < ApplicationController
     @sharednotes = Sharenote.all.includes(:note, :user).where(
       user_id: current_user.id
     ).where(notes: { status: false })
-    render plain: @hsraednotes.inspect
+    render plain: @sharednotes.inspect
   end
 
   #
