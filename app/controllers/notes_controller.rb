abf98b621989319acdbc421dfc5fc7ee307a68f8
dg-narola <dg@narola.email>
2019-03-12 15:35:50 +0530

    new

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -30,7 +30,7 @@ class NotesController < ApplicationController
     # note = Note.search_published(query)
     # render plain: note.inspect
     @notes =
-      if params[:search_notes].presence && params[:search_notes][:query]
+      if params[:query]
         @notes = Note.includes(:tags,:taggings).references(:tags).where(
           'notes.title LIKE :search OR notes.description LIKE :search OR tags.name LIKE :search',
           search: "%#{params[:query]}%"
@@ -39,7 +39,7 @@ class NotesController < ApplicationController
          user_id: current_user.id
        ).distinct
       end
-     # render plain: @notes.inspect
+      render plain: @notes.inspect
   end
 
   #
